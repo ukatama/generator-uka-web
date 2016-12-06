1af90a29908fac139b3288e688f2c7002db5b841
ukatama <dev.ukatama@gmail.com>
2016-12-07 00:17:41 +0900

    [fix] Lint

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,37 @@
 'use strict';
+
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the phenomenal ' + chalk.red('generator-uka-web') + ' generator!'
-    ));
+    prompting: function () {
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the phenomenal ' + chalk.red('generator-uka-web') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        var prompts = [{
+            type: 'confirm',
+            name: 'someAnswer',
+            message: 'Would you like to enable this option?',
+            default: true
+        }];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+        return this.prompt(prompts).then(function (props) {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        }.bind(this));
+    },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
+    writing: function () {
+        this.fs.copy(
+            this.templatePath('dummyfile.txt'),
+            this.destinationPath('dummyfile.txt')
+        );
+    },
 
-  install: function () {
-    this.installDependencies();
-  }
+    install: function () {
+        this.installDependencies();
+    }
 });
